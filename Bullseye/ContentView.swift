97e62beb220f6ffa0cd4774a12d6609e0b37e03c
Vlad Stanescu <vladstanescu94@gmail.com>
2021-01-12 14:16:43 +0200

    Add pop-up alert

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -8,14 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var alertIsVisible: Bool = false
+    
     var body: some View {
         VStack {
             Text("Welcome to my first app!")
                 .fontWeight(.semibold)
                 .foregroundColor(Color.green)
                 .padding()
-            Button(action: {}) {
+            Button(action: {
+                print("Button pressed!")
+                alertIsVisible.toggle()
+            }) {
                 Text("Hit me!")
+            }
+            .alert(isPresented: $alertIsVisible) { () -> Alert in
+                return Alert(title: Text("Hello there!"), message: Text("This is my first pop-up"), dismissButton: .default(Text("Awesome!")))
             }
         }
     }
