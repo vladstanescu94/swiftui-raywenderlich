699f36db7a89bdc3bc0cf47c71ed7778095b7f78
Vlad Stanescu <vladstanescu94@gmail.com>
2021-01-12 13:16:31 +0200

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Vlad Stanescu on 12.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
